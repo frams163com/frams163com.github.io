10ea64117da4567780430e27546e51363d16bb29
frams163com <frams@163.com>
2016-12-05 11:49:06 +0800

    1

---
+++ drivers/HexGLzombie/geometries/ships/feisar/feisar-jychen.js
@@ -0,0 +1,41 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "feisar.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 5,
+        "faces"         : 1,
+        "normals"       : 1,
+        "colors"        : 0,
+        "uvs"           : 5,
+        "materials"     : 1
+    },
+
+    "scale" : 0.200000,
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "_auto_",
+	"colorDiffuse" : [0.235294, 0.223529, 0.188235],
+	"colorSpecular" : [0.33, 0.33, 0.33],
+	"mapDiffuse" : "_auto_.png"
+	}],
+
+    "vertices": [0.098824,0.050000,-0.663979,0.098824,0.842856,-0.663979,0.587233,1.721050,-0.663979,1.160970,1.721050,-0.663979,1.160970,0.050000,-0.663979],
+
+    "morphTargets": [],
+
+    "morphColors": [],
+
+    "normals": [5.8555e-17,3.9771e-16,-1],
+
+    "colors": [],
+
+    "uvs": [[-2.9856,-0.0020667,-2.9856,0.47088,-3.4523,0.99474,-4.0005,0.99474,-4.0005,-0.0020667]],
+
+    "faces": [43,0,1,2,3,0,0,1,2,3,0,0,0,0]
+
+}
